16bef973c78eefa86b8f345f8b5bf2f9c28c3184
Marcus Vinicius <121181021+marcussviniicius@users.noreply.github.com>
2025-05-23 20:18:53 -0300

    Update cifra.js

--- Cifra da Nlogônia/cifra.js
+++ Cifra da Nlogônia/cifra.js
@@ -1,8 +1,6 @@
 function cifrar(palavra){
-    const alfabeto = 
-    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "z"] //alfabeto da questão
-    const vogais = 
-    ["a", "e", "i", "o", "u"]
+    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "z"] //alfabeto da questão
+    const vogais = ["a", "e", "i", "o", "u"]
     const letras = palavra.split("")
     let palavra_cifrada = new Array()
 
